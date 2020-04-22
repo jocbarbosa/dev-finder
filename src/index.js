daf754483cc48e533cf31aa1fd11953f5726267f
Joao Otavio Chrisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-04-22 15:43:34 -0300

    feat: avoid creation of duplicate dev

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-mlbks.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://omnistack:omnistack@cluster0-mlbks.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
