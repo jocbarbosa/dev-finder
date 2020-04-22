3517e0e2fe1838e97d4621d6f7d6fbbe82a57cad
Joao Otavio Chrisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-04-22 15:32:57 -0300

    feat: Dev creation added
    Controller and model were created for users

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://omnistack:${process.env.CLUSTER_PASSWORD}@cluster0-mlbks.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-mlbks.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.use(express.json());
+app.use(routes);
+
 
 app.listen(3333);
-
-app.get('/users', (request, response) => {
-    const { id, name } = request.body;
-
-    response.json({
-        id: id,
-        name: name
-    });
-});
